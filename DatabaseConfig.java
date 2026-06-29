8e3acb3e32c8f964b4aa371226b9195b2e62c700
vevatarigan-source <vevatarigan@gmail.com>
2026-06-29 20:22:14 +0700

    Add files via upload

---
+++ DatabaseConfig.java
@@ -0,0 +1,14 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConfig{
+
+    private static final String URL = "jdbc:mysql://localhost:3306/db_game";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
